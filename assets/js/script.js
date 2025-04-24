71d34162c19dabeaab66ef9dd13872586fc360f9
MoizPatvi-Lab <moiz.bid@gmail.com>
2025-04-24 15:47:19 -0500

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["PowerBI", "Tableau", "Data Modeling", "Data Visualization", "Data Storytelling", "SQL & T-SQL"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
