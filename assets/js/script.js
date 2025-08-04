51a5c0dc9dd3ee1aa8fcb9e7bafead223496fdde
muhamadimamfa <imamfahurrozi@gmail.com>
2025-08-04 18:19:11 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,9 +77,9 @@ var typed = new Typed(".typing-text", {
 async function fetchData(type = "skills") {
     let response;
     type === "skills" ?
-        response = await fetch("../skills.json")
+        response = await fetch("skills.json")
         :
-        response = await fetch("../projects.json");
+        response = await fetch("projects.json");
     const data = await response.json();
     return data;
 }
@@ -102,29 +102,44 @@ function showSkills(skills) {
 function showProjects(projects) {
     let projectsContainer = document.querySelector("#work .box-container");
     let projectHTML = "";
-    projects.slice(0, 10).filter(project => project.category != "android").forEach(project => {
+    projects.forEach(project => {
         projectHTML += `
-        <div class="box tilt">
-          <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="${project.name}" />
-          <div class="content">
-            <div class="tag">
-              <h3>${project.name}</h3>
+        <div class="box tilt ${project.category}">
+            <img draggable="false" src="assets/images/projects/${project.image}.png" alt="${project.name}" />
+            <div class="content">
+                <div class="tag">
+                    <h3>${project.name}</h3>
+                </div>
+                <div class="desc">
+                    <p>${project.desc}</p>
+                    <div class="btns">
+                        <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+                        <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+                    </div>
+                </div>
             </div>
-            <div class="desc">
-              <p>${project.desc}</p>
-              <div class="btns">
-                <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-                <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
-              </div>
-            </div>
-          </div>
         </div>`
     });
     projectsContainer.innerHTML = projectHTML;
 
-    // vanilla tilt.js
     VanillaTilt.init(document.querySelectorAll(".tilt"), {
         max: 15,
+    });
+    
+    // isotope filter products
+    var $grid = $('.box-container').isotope({
+        itemSelector: '.box',
+        layoutMode: 'fitRows',
+        masonry: {
+            columnWidth: 200
+        }
+    });
+
+    $('.button-group').on('click', 'button', function () {
+        $('.button-group').find('.is-checked').removeClass('is-checked');
+        $(this).addClass('is-checked');
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
     });
 }
 
