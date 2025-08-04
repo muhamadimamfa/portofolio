b8a0acd93737e1eb354378dd109f5fa72cf56fac
muhamadimamfa <imamfahurrozi@gmail.com>
2025-08-04 13:40:21 +0700

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -105,7 +105,7 @@ function showProjects(projects) {
     projects.slice(0, 10).filter(project => project.category != "android").forEach(project => {
         projectHTML += `
         <div class="box tilt">
-          <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="${project.name}" />
+          <img draggable="false" src="./assets/images/projects/${project.image}.png" alt="${project.name}" />
           <div class="content">
             <div class="tag">
               <h3>${project.name}</h3>
@@ -205,3 +205,4 @@ srtop.reveal('.experience .timeline .container', { interval: 400 });
 
 srtop.reveal('.contact .container', { delay: 400 });
 srtop.reveal('.contact .container .form-group', { delay: 400 });
+
